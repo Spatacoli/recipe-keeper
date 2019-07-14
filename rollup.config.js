893bc848cbd8b0c62c5345a362fc6775e794a075
Todd Spatafore <todd@thetodd.com>
2019-07-14 16:36:00 -0700

    Adding Rollup.js to the build process

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import json from "rollup-plugin-json";
+
+module.exports = {
+    input: 'src/javascript/main.js',
+    output: {
+      file: 'dist/javascript/bundle.js',
+      format: 'iife',
+      name: 'spatafore'
+    },
+    plugins: [ json() ]
+  };
---
+++ src/javascript/main.js
@@ -0,0 +1,5 @@
+import recipes from "../../data/recipes.json";
+
+export default function() {
+    console.log(recipes);
+}
